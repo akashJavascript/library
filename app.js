d0cf26c6b039e8cf8166b37db9d81fe4a6d668a4
Akash <akashrangaswamy2010@gmail.com>
2023-12-18 10:33:37 -0800

    Add book to library arr functional

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+const myLibrary = [];
 function Book(title,author,pages,readStatus){
     this.title = title;
     this.author= author;
     this.pages = pages;
     this.readStatus = readStatus;
-    this.info = function(){
-    return `${this.title} by ${this.author}, ${pages} pages, ${readStatus ? 'have read' : 'not read yet'}`
-    }
 }
-let theHobbit = new Book("The Hobbit","J.R.R. Tolkien",295,false)
-console.log(theHobbit.info())
+function addBookToLibrary(title,author,pages,readStatus) {
+    let book = new Book(title,author,pages,readStatus);
+    myLibrary.push(book)
+    return book;
+}
+
+let hobbit = addBookToLibrary("The Hobbit","J.R.R. Tolkien",295,false)
+let b = addBookToLibrary('abcdfdfdf',"ddd,,d,d,d",545,true)
+console.log(hobbit)
+console.log(myLibrary)
