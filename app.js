a87df2aaf29e1f00c2bf8d5eca417b02a32cbf68
Akash <akashrangaswamy2010@gmail.com>
2023-12-18 14:20:07 -0800

    Dialog css finished

--- app.js
+++ app.js
@@ -1,45 +1,52 @@
-const myLibrary = [];
+document.addEventListener("DOMContentLoaded", function () {
+    const myLibrary = [];
+    const dialog = document.querySelector('.dialog');
+    const addBookBtn = document.querySelector('.newBook');
 
-function Book(title, author, pages, readStatus) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.readStatus = readStatus;
-}
+    addBookBtn.addEventListener('click', function () {
+        dialog.classList.remove('dialogHidden');
+        dialog.classList.add('dialogShow');
+    });
 
-function addBookToLibrary(title, author, pages, readStatus) {
-  let book = new Book(title, author, pages, readStatus);
-  myLibrary.push(book);
-  return book;
-}
+    function Book(title, author, pages, readStatus) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus;
+    }
 
-function createAndAppendElement(parent, textContent, className) {
-  let element = document.createElement('div');
-  element.textContent = textContent;
-  element.classList.add(className);
-  parent.appendChild(element);
-}
+    function addBookToLibrary(title, author, pages, readStatus) {
+        let book = new Book(title, author, pages, readStatus);
+        myLibrary.push(book);
+        return book;
+    }
 
-function displayBooks(myLibrary) {
-  let container = document.querySelector('.bookContainer');
-  container.innerHTML = ''; // Clear the existing content of the container
+    function createAndAppendElement(parent, textContent, className) {
+        let element = document.createElement('div');
+        element.textContent = textContent;
+        element.classList.add(className);
+        parent.appendChild(element);
+    }
 
-  myLibrary.forEach(book => {
-    let newCard = document.createElement('div');
-    newCard.classList.add('card');
+    function displayBooks(myLibrary) {
+        let container = document.querySelector('.bookContainer');
+        container.innerHTML = ''; // Clear the existing content of the container
 
-    // Create and append elements for title, author, pages, and readStatus
-    createAndAppendElement(newCard, `"${book.title}"`, 'elInCard');
-    createAndAppendElement(newCard, book.author, 'elInCard');
-    createAndAppendElement(newCard, `${book.pages} pages`, 'elInCard');
-    createAndAppendElement(newCard, `${book.readStatus?'read':'not read yet'}`, 'elInCard');
+        myLibrary.forEach(book => {
+            let newCard = document.createElement('div');
+            newCard.classList.add('card');
 
-    container.appendChild(newCard);
-  });
-}
+            // Create and append elements for title, author, pages, and readStatus
+            createAndAppendElement(newCard, `"${book.title}"`, 'elInCard');
+            createAndAppendElement(newCard, book.author, 'elInCard');
+            createAndAppendElement(newCard, `${book.pages} pages`, 'elInCard');
+            createAndAppendElement(newCard, `${book.readStatus ? 'read' : 'not read yet'}`, 'elInCard');
 
-let hobbit = addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
-let b = addBookToLibrary('abcdfdffdfdfdfdffdfsfdsfdsfdsfsdfffsdfds44334fddfdfdffddfdfdfdf', 'ddd,,d,d,d', 545, true);
-console.log(hobbit);
-displayBooks(myLibrary);
-console.log(myLibrary);
+            container.appendChild(newCard);
+        });
+    }
+
+    let hobbit = addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+    let b = addBookToLibrary('abcdfdffdfdfdfdffdfsfdsfdsfdsfsdfffsdfds44334fddfdfdffddfdfdfdf', 'ddd,,d,d,d', 545, true);
+    displayBooks(myLibrary);
+});
